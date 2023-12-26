b9e9cd221f95310006f958888ae2aa8b473e09bc
Yosef mare <yosefmare108@gmail.com>
2023-12-26 10:51:36 +0200

    add sass and tailwind css

--- frontend/produts-stroe/src/main.tsx
+++ frontend/produts-stroe/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { RouterProvider } from "react-router-dom";
 import router from './routes/routes.tsx';
+import './main.scss'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
