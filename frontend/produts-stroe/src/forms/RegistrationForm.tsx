81488e5f1a633d0e8852e2a5de9ff47df1ed3018
Yosef mare <yosefmare108@gmail.com>
2023-12-31 10:59:59 +0200

    create login and register handler

--- frontend/produts-stroe/src/forms/RegistrationForm.tsx
+++ frontend/produts-stroe/src/forms/RegistrationForm.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
-import { createEntity } from "../utils/utils";
-
 import { useState } from 'react';
+import { sendFormData } from "../handelers/handelers";
 const RegistrationFrom = () => {
 
   const [formData, setFormData] = useState({
@@ -11,34 +10,21 @@ const RegistrationFrom = () => {
     role: "",
   });
 
-  const sendFormData = async () => {
-const userData = await createEntity('http://localhost:8000/register', formData)
-if (userData) {
-  console.log(userData);
-  const {data} = userData
-  const {newUser} = data
-  const userInfo = JSON.stringify({userName: newUser.userName, profileImg: newUser.profileImg})
-  localStorage.setItem('userInfo', userInfo)
-  localStorage.setItem('token', userData.token)
-
-}
-  }
-
   return (
     <form className=" flex flex-col gap-6">
       <h1 className=" text-center font-medium text-xl">Register</h1>
-      <input onChange={(e) => setFormData({...formData, userName: e.target.value})} className=" rounded-sm px-6 py-1" type="text" placeholder="userName " />
-      <input onChange={(e) => setFormData({...formData, email: e.target.value})} className=" rounded-sm px-6 py-1" type="text" placeholder="Email " />
-      <input onChange={(e) => setFormData({...formData, password: e.target.value})} className=" rounded-sm px-6 py-1" type="password" placeholder="Password" />
-      <select onChange={(e) => setFormData({...formData, role: e.target.value})} className=" select rounded-sm px-6 py-1">
+      <input onChange={(e) => setFormData({ ...formData, userName: e.target.value })} className=" rounded-sm px-6 py-1" type="text" placeholder="userName " />
+      <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} className=" rounded-sm px-6 py-1" type="text" placeholder="Email " />
+      <input onChange={(e) => setFormData({ ...formData, password: e.target.value })} className=" rounded-sm px-6 py-1" type="password" placeholder="Password" />
+      <select onChange={(e) => setFormData({ ...formData, role: e.target.value })} className=" select rounded-sm px-6 py-1">
         <option hidden>role</option>
         <option>admin</option>
         <option>user</option>
       </select>
       <button onClick={(e) => {
-        e.preventDefault() 
-        sendFormData()
-        }} className=" p-2 bg-sky-600 text-white cursor-pointer active:bg-sky-700">Register</button>
+        e.preventDefault()
+        sendFormData('http://localhost:8000/auth/register', formData)
+      }} className=" p-2 bg-sky-600 text-white cursor-pointer active:bg-sky-700">Register</button>
       <div className=" text-center">
         <Link to={'/login'} className=" text-sky-600 underline font-medium">Login</Link>
       </div>
