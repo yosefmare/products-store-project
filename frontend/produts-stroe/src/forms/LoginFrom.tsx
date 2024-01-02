f50effc18ab4bbc3010042c820d331351d841a9f
Yosef mare <yosefmare108@gmail.com>
2024-01-02 07:06:49 +0200

    add the sendFormData handler to LoginForm Component

--- frontend/produts-stroe/src/forms/LoginFrom.tsx
+++ frontend/produts-stroe/src/forms/LoginFrom.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
-
-
+import { sendFormData } from "../handelers/handelers";
+import { useState } from 'react';
 
 const LoginAndRegisterForm = (): JSX.Element => {
+    
+    const [formData, setFormData] = useState({email: "", password: ''});
+    
     return (
         <form className=" flex flex-col gap-6">
             <h1 className=" text-center font-medium text-xl">Login</h1>
-            <input className=" rounded-sm px-6 py-1" type="text" placeholder="Email " />
-            <input className=" rounded-sm px-6 py-1" type="password" placeholder="Password" />
-            <button onClick={(e) => e.preventDefault()} className=" p-2 bg-sky-600 text-white cursor-pointer active:bg-sky-700">Login</button>
+            <input onChange={(e) => setFormData({...formData, email: e.target.value})} className=" rounded-sm px-6 py-1" type="text" placeholder="Email " />
+            <input onChange={(e) => setFormData({...formData, password: e.target.value})} className=" rounded-sm px-6 py-1" type="password" placeholder="Password" />
+            <button onClick={(e) => {
+                e.preventDefault()
+                sendFormData('http://localhost:8000/auth/login', formData)
+            }} className=" p-2 bg-sky-600 text-white cursor-pointer active:bg-sky-700">Login</button>
             <div className=" text-center">
                 <Link to={'/register'} className=" text-sky-600 underline font-medium">Register</Link>
             </div>
