b6711d5e44126e6b0b2aaba377ff64652b38adb8
Yosef mare <yosefmare108@gmail.com>
2024-01-05 11:41:23 +0200

    merge the token when user data parsed in the sendFormData handler

--- frontend/produts-stroe/src/handelers/handelers.ts
+++ frontend/produts-stroe/src/handelers/handelers.ts
@@ -11,9 +11,22 @@ export const sendFormData = async (url: string, state: sendFormDataStateTypes) =
     const userData = await createEntity(url, state)
     if (userData) {
         console.log(userData);
-        const { user } = userData
+        const { user, token } = userData
         const userInfo = JSON.stringify({ userName: user.userName, profileImg: user.profileImg })
         localStorage.setItem('userInfo', userInfo)
-        localStorage.setItem('token', userData.token)
+        if (token) {
+            const userToken = JSON.stringify({ token })
+            localStorage.setItem('token', userToken)
+        }
+    }
+}
+
+export const getUserDataFromLocalStorage = () => {
+    const userDataFomLocalStorage = localStorage.getItem('userInfo')
+    const token = localStorage.getItem('token')
+    if (userDataFomLocalStorage && token) {
+        const parsedUserData = JSON.parse(userDataFomLocalStorage)
+        const parsedToken = JSON.parse(token)
+        return { ...parsedUserData, token: parsedToken }
     }
 }
