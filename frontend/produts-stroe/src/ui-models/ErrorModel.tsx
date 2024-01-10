6364070d958e32a34074947a75748da1df59421d
Yosef mare <yosefmare108@gmail.com>
2024-01-10 16:14:52 +0200

    add type to ErrorModel component

--- frontend/produts-stroe/src/ui-models/ErrorModel.tsx
+++ frontend/produts-stroe/src/ui-models/ErrorModel.tsx
@@ -4,7 +4,7 @@ type PropsTypes = {
     display: boolean
 }
 
-const ErrorModel = ({message, display}: PropsTypes) => {
+const ErrorModel = ({message, display}: PropsTypes):JSX.Element => {
     return (
         <div className={`bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 ${display? "block": "hidden"}`} role="alert">
             <strong className="font-bold">Error! </strong>
