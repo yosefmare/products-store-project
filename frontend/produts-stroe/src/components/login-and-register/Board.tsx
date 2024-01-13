4189d0e87ceac0f861177205185995d6d5e009d6
Yosef mare <yosefmare108@gmail.com>
2024-01-13 13:26:45 +0200

    edit board size

--- frontend/produts-stroe/src/components/login-and-register/Board.tsx
+++ frontend/produts-stroe/src/components/login-and-register/Board.tsx
@@ -6,8 +6,8 @@ type propsType = {
 
 const Board = ({ children }: propsType): JSX.Element => {
     return (
-        <div className="flex items-center justify-center h-[100vh]">
-            <div className="lg:w-96 h-96 flex flex-col  shadow-md rounded-md bg-gray-300">
+        <div className="flex items-center justify-center w-[100wv] h-[100vh]">
+            <div className=" max-h-[32rem] w-96 flex flex-col  shadow-md rounded-md bg-gray-300">
                 {children}
             </div>
         </div>
