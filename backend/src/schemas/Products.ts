4d1c2edda9ee6dc109d156d789d893e9bcd4a060
Yosef mare <yosefmare108@gmail.com>
2024-01-18 17:37:36 +0200

    add category filled to the products schema

--- backend/src/schemas/Products.ts
+++ backend/src/schemas/Products.ts
@@ -3,12 +3,15 @@ import { ProductsDocuments } from '../types/UserAndRegisrationMethodeTypes';
 
 
 const ProductsSchema = new Schema({
-    name: {type:String, required: true},
-    price: {type:Number, required: true},
-    quantity: {type:Number, required: true},
-    productImg: {type:String, required: true}
-}, {versionKey: false})
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    productImg: { type: String, required: true },
+    category: [
+        { type: String, required: true }
+    ]
+}, { versionKey: false })
 
-const ProductsModal = model <ProductsDocuments>('products', ProductsSchema)
+const ProductsModal = model<ProductsDocuments>('products', ProductsSchema)
 
 export default ProductsModal
