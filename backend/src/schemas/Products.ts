5580f62a10a761f36fa64316e5654e98db5a7c77
Yosef mare <yosefmare108@gmail.com>
2024-02-07 14:01:13 +0200

    validate the category array

--- backend/src/schemas/Products.ts
+++ backend/src/schemas/Products.ts
@@ -7,10 +7,18 @@ const ProductsSchema = new Schema({
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
     productImg: { type: String, required: true },
-    category: [
-        { type: String, required: true }
-    ]
-}, { versionKey: false })
+    category: {
+        type: [{
+            type: String,
+            required: true
+        }],
+        validate: [categoryLengthValidator, 'Category array length must be at least 1 and at most 5']
+    }
+}, { versionKey: false });
+
+function categoryLengthValidator(value: string) {
+    return value.length >= 1 && value.length <= 3;
+}
 
 const ProductsModal = model<ProductsDocuments>('products', ProductsSchema)
 
