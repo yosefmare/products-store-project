d60698095ee54ba48f748758cbfc7a62cf6f3c42
Yosef mare <yosefmare108@gmail.com>
2024-02-07 14:07:29 +0200

    checks the response

--- frontend/produts-stroe/src/features/api/productsAsyncThunk.api.ts
+++ frontend/produts-stroe/src/features/api/productsAsyncThunk.api.ts
@@ -3,11 +3,31 @@ import { createEntity, getAllEntity } from "../../utils/utils"
 import { AddProductFunctionObject } from "../../types/globalTypes"
 
 export const getAllProduts = createAsyncThunk('products/getAllProduts', async () => {
-    const {data} = await getAllEntity('http://localhost:8000/products/getAllProducts')
-    return data
+    try {
+        const { data, response } = await getAllEntity('http://localhost:8000/products/getAllProducts')
+    if (data) {
+        console.log(data);
+        return data
+    } else {
+        throw new Error(response.data.message)
+    }
+    } catch (error) {
+        const fetchError = error instanceof Error ? error : new Error('fetch failed')
+        throw fetchError
+    }
 })
-export const addProduct = createAsyncThunk('products/addProduts', async ({productData, headers}: AddProductFunctionObject ) => {
-    const {data} = await createEntity('http://localhost:8000/products/addProduct', productData, headers)
-    console.log(data);
-    return data
+
+export const addProduct = createAsyncThunk('products/addProduts', async ({ productData, headers }: AddProductFunctionObject) => {
+    try {
+        const { data, response } = await createEntity('http://localhost:8000/products/addProduct', productData, headers)
+        if (data) {
+            console.log(data);
+            return data
+        } else {
+            throw new Error(response.data.message)
+        }
+    } catch (error) {
+        const fetchError = error instanceof Error ? error : new Error('fetch failed')
+        throw fetchError
+    }
 })
