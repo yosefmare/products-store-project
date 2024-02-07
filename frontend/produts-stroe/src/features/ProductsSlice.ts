cb36e85645a69c0c96c9d0f9c483aa1f65e6f135
Yosef mare <yosefmare108@gmail.com>
2024-02-07 14:03:09 +0200

    handling the request status and fix some bugs

--- frontend/produts-stroe/src/features/ProductsSlice.ts
+++ frontend/produts-stroe/src/features/ProductsSlice.ts
@@ -12,17 +12,19 @@ interface Products {
 
 interface ProdutsState {
     products: Products[];
+    showResponseMessage: boolean;
     loading: boolean;
-    error: null | string;
+    error: string | undefined | null; 
+    success: string;
 }
 
 const initialState: ProdutsState = {
     products: [],
+    showResponseMessage: false,
     loading: false,
-    error: null
-
+    error: null,
+    success: ''
 }
-
 
 
 export const productsSlice = createSlice({
@@ -37,13 +39,20 @@ export const productsSlice = createSlice({
         })
 
         builder
-        .addCase(addProduct.pending, (state) => {
-            state.loading = true
-        })
-        .addCase(addProduct.fulfilled, (state, action) => {
-            state.loading = false
-            state.products.push(action.payload)
-        })
+            .addCase(addProduct.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(addProduct.fulfilled, (state, action) => {
+                state.loading = false
+                state.error = null
+                state.success = action.payload.message
+                state.showResponseMessage = true
+            })
+            .addCase(addProduct.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.error.message
+                state.showResponseMessage = true
+            })
     }
 })
 
