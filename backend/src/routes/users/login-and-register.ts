8328b4a3c06ac8c46d04832f9a0453f42117aa35
Yosef mare <yosefmare108@gmail.com>
2024-05-18 12:23:07 +0300

    add multer middlewere to login and register routes

--- backend/src/routes/users/login-and-register.ts
+++ backend/src/routes/users/login-and-register.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { login, register } from "../../utils/auth";
+import { upload } from "../../file-managment/multer";
 
 const router = Router()
-router.post('/auth/login', login)
-    .post('/auth/register', register)
+router
+    .post('/auth/login', upload.none(), login)
+    .post('/auth/register', upload.none(), register)
 
 export default router
