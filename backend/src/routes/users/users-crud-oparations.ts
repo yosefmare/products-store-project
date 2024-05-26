8a584b1503f142a85ec9c6579014d6533794766a
Yosef mare <yosefmare108@gmail.com>
2024-05-26 23:13:10 +0300

    aligning

--- backend/src/routes/users/users-crud-oparations.ts
+++ backend/src/routes/users/users-crud-oparations.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { updateEntity } from "../../utils/utils";
 import UserModel from "../../schemas/Users";
 import { Request, Response } from 'express'
-import {upload, setImageProfile} from "../../file-managment/multer";
+import { upload, setImageProfile } from "../../file-managment/multer";
 
-        const router = Router()
-        router.post('/users/uploadProfileImage', upload.single('file'), (req:Request, res:Response) =>  setImageProfile(req, res, UserModel))
-        router.patch('/users/editProfile/:id', (req: Request, res: Response) => updateEntity(req, res, UserModel))
+const router = Router()
+router.post('/users/uploadProfileImage', upload.single('file'), (req: Request, res: Response) => setImageProfile(req, res, UserModel))
+router.patch('/users/editProfile/:id', (req: Request, res: Response) => updateEntity(req, res, UserModel))
 
-    
+
 
 
 export default router
