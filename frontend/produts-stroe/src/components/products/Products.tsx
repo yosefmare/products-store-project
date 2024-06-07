c55c5adca25e6d397e2d0d33f220ca9698fe83ec
Yosef mare <yosefmare108@gmail.com>
2024-06-07 15:17:08 +0300

    create search logic

--- frontend/produts-stroe/src/components/products/Products.tsx
+++ frontend/produts-stroe/src/components/products/Products.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { getAllProduts } from "../../features/api/productsAsyncThunk.api";
 import Spinner from "../../ui-models/Spinner";
@@ -8,11 +8,15 @@ import ProductCard from "./ProductCard";
 const Products = () => {
   const products = useAppSelector((state) => state.productsSlice)
   const dispatch = useAppDispatch()
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    dispatch(getAllProduts())
-    console.log(products.products);
-  }, []);
+    dispatch(getAllProduts());
+  }, [dispatch]);
+
+  const filteredProducts = products.products.filter(product =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
@@ -21,13 +25,23 @@ const Products = () => {
         <input
           type="text"
           placeholder="Search products..."
-          className=" ml-14 p-2 border border-gray-300 rounded-lg w-1/2 sm:w-1/4 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="ml-14 p-2 border border-gray-300 rounded-lg w-1/2 sm:w-1/4 bg-gray-100 focus:outline-none focus:ring-2 focus:ring-slate-800"
         />
-        <Link to={'/products/AddProduct'} className="btn h-12 w-12 rounded-full flex items-center justify-center font-bold mx-2 sm:mx-10">+</Link>
+        <Link to={'/products/AddProduct'} className="btn font-bold text-xl h-12 w-12 rounded-full flex items-center justify-center mx-2 sm:mx-10">+</Link>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 px-4 sm:px-12 mt-5">
         {
-          products.products.map((product) => <ProductCard key={product._id} id={product._id} productImg={product.productImg} name={product.name} price={product.price} />)
+          filteredProducts.map((product) => (
+            <ProductCard
+              key={product._id}
+              id={product._id}
+              productImg={product.productImg}
+              name={product.name}
+              price={product.price}
+            />
+          ))
         }
       </div>
     </div>
