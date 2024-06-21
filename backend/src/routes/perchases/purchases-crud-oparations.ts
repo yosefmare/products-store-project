6358b35f3f36454eb5f80c00f44e01b709c7b342
Yosef mare <yosefmare108@gmail.com>
2024-06-21 20:24:16 +0300

    protect get all and by id purchese

--- backend/src/routes/perchases/purchases-crud-oparations.ts
+++ backend/src/routes/perchases/purchases-crud-oparations.ts
@@ -1,12 +1,18 @@
 import { getAllEntities, getEntityById, createEntity, updateEntity, deleteEntity } from "../../utils/utils";
 import PurchasesModel from "../../schemas/Purchases";
 import { Request, Response, Router } from 'express'
+import { protectionRoutesHandler } from "../../utils/auth";
 
-    const router = Router()
-    router.get('/purchases/getAllPurchases', (req: Request, res: Response) => getAllEntities(req, res, PurchasesModel, ['product', 'customer']))
-        .get('/purchases/getPurchase/:id', (req: Request, res: Response) => getEntityById(req, res, PurchasesModel, ['product', 'customer']))
-        .post('/purchases/addPurchases', (req: Request, res: Response) => createEntity(req, res, PurchasesModel))
-        .delete('/purchases/deletePurchases/:id', (req: Request, res: Response) => deleteEntity(req, res, PurchasesModel))
-        .patch('/purchases/editPurchases/:id', (req: Request, res: Response) => updateEntity(req, res, PurchasesModel))
-        
+const router = Router()
+router
+    .get('/purchases/getAllPurchases', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, getAllEntities.bind(null, req, res, PurchasesModel, ['product', 'customer']))
+    })
+    .get('/purchases/getPurchase/:id', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, getEntityById.bind(null, req, res, PurchasesModel, ['product', 'customer']))
+    })
+    .post('/purchases/addPurchases', (req: Request, res: Response) => createEntity(req, res, PurchasesModel))
+    .delete('/purchases/deletePurchases/:id', (req: Request, res: Response) => deleteEntity(req, res, PurchasesModel))
+    .patch('/purchases/editPurchases/:id', (req: Request, res: Response) => updateEntity(req, res, PurchasesModel))
+
 export default router
