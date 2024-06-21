46d4f5229d50d3b37dd37208f4bb022f7afc35b8
Yosef mare <yosefmare108@gmail.com>
2024-06-21 16:08:38 +0300

    protect get all and by id customers. cancel create customers protection

--- backend/src/routes/customers/customrs-crud-oparations.ts
+++ backend/src/routes/customers/customrs-crud-oparations.ts
@@ -4,19 +4,21 @@ import CustomersModel from "../../schemas/Customers";
 import { Request, Response } from 'express'
 import { protectionRoutesHandler } from "../../utils/auth";
 
-        const router = Router()
-        router.get('customers/getAllCustomers', (req: Request, res: Response) => getAllEntities(req, res, CustomersModel))
-        .get('customers/getCustomer/:id', (req: Request, res: Response) => getEntityById(req, res, CustomersModel))
-
-        .post('customers/addCustomer', (req: Request, res: Response) => {
-            protectionRoutesHandler(req, res, createEntity.bind(null, req, res, CustomersModel));
-        })
-        .patch('customers/updateCustomer/:id', (req: Request, res: Response) => {
-            protectionRoutesHandler(req, res, updateEntity.bind(null, req, res, CustomersModel))
-        })
-        .delete('customers/deleteCustomer/:id', (req: Request, res: Response) => {
-            protectionRoutesHandler(req, res, deleteEntity.bind(null, req, res, CustomersModel))
-        })
+const router = Router()
+router
+    .get('/customers/getAllCustomers', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, getAllEntities.bind(null, req, res, CustomersModel))
+    })
+    .get('/customers/getCustomer/:id', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, getEntityById.bind(null, req, res, CustomersModel))
+    })
+    .post('/customers/addCustomer', (req: Request, res: Response) => createEntity(req, res, CustomersModel))
+    .patch('/customers/updateCustomer/:id', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, updateEntity.bind(null, req, res, CustomersModel))
+    })
+    .delete('/customers/deleteCustomer/:id', (req: Request, res: Response) => {
+        protectionRoutesHandler(req, res, deleteEntity.bind(null, req, res, CustomersModel))
+    })
 
 
 export default router
