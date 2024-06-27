2e8b9f6ef5e100b32782bd7748cdeabda61298a4
Yosef mare <yosefmare108@gmail.com>
2024-06-27 23:54:13 +0300

    add user filed to customer schema to link the customer to the user that subscribed as a customer

--- backend/src/schemas/Customers.ts
+++ backend/src/schemas/Customers.ts
@@ -1,8 +1,14 @@
 import { Schema, model } from 'mongoose';
 import { CustomersDocument } from '../types/UserAndRegisrationMethodeTypes';
 
+const ObjectId = Schema.Types.ObjectId
+
 
 const CustomersSchema = new Schema({
+    user:{
+        type: ObjectId,
+        ref: 'customers'
+    },
     firstName: String,
     lastName: String,
     city: String
