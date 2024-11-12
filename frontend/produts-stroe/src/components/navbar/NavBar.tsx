b6c35906eea9e4fe9b675baa0d8893985f65f8bd
Yosef Mare <yosefmare108@gmail.com>
2024-11-12 13:05:58 +0200

    create Function to determine which profile image to set is the navbar base on user

--- frontend/produts-stroe/src/components/navbar/NavBar.tsx
+++ frontend/produts-stroe/src/components/navbar/NavBar.tsx
@@ -4,43 +4,61 @@ import ShoppingCard from './ShoppingCard';
 import LogOut from '../login-and-register/LogOut';
 import { loadUserDataFromLocalStorage } from '../../utils/utils';
 
-const defaultProfileImage: string = '/default-profile-image/default-profile-image.webp'
-const userData = loadUserDataFromLocalStorage()
+const defaultProfileImage: string = 'default-profile-image/default-profile-image.webp';
+const userData = loadUserDataFromLocalStorage();
 const navLinks = [
     { content: 'products', path: '/products' },
     { content: 'customers', path: '/customers' },
     { content: 'purchases', path: '/purchases' }
 ];
 
+// Function to determine which profile image to use
+
 const NavBar = (): JSX.Element => {
-    const authSlice = useAppSelector((state) => state.authSlice.auth)
+    const authSlice = useAppSelector((state) => state.authSlice.auth);
+
+    const setProfileImage = (): string => {
+        if (authSlice?.profileImg) {
+            return authSlice.profileImg;
+        } else if (userData?.profileImg) {
+            return userData.profileImg;
+        } else {
+            return defaultProfileImage;
+        }
+    };
 
     return (
-        <div className='flex justify-between py-4 $'>
+        <div className='flex justify-between py-4'>
 
             {/* features */}
             <div className='flex gap-2 items-center justify-center'>
-            <ShoppingCard />
-            <LogOut/>
+                <ShoppingCard />
+                <LogOut />
             </div>
+
             {/* main nav content */}
-            {authSlice?.role == 'admin' || userData?.role == 'admin'
-                ?
+            {authSlice?.role === 'admin' || userData?.role === 'admin' ? (
                 <ul className='flex gap-10'>
-                    {
-                        navLinks.map((content, index) => <li key={index} className='btn'><Link to={content.path}>{content.content}</Link></li>)
-                    }
+                    {navLinks.map((content, index) => (
+                        <li key={index} className='btn'>
+                            <Link to={content.path}>{content.content}</Link>
+                        </li>
+                    ))}
                 </ul>
-                :
-                <></>
-            }
+            ) : null}
 
             {/* user info */}
             <div>
                 <div className='flex gap-2 justify-center items-center mr-4'>
-                    <h3 className=' font-bold'>{userData?.userName}</h3>
+                    <h3 className='font-bold'>
+                        {authSlice?.userName ? authSlice.userName : userData?.userName}
+                    </h3>
                     <Link to={'/user/profile/updateImage'}>
-                        <img className='w-10 h-10 rounded-full cursor-pointer' src={`${userData?.profileImg ? `/${userData?.profileImg}` : defaultProfileImage}`} alt="profileImg" />
+                        <img
+                            className='w-10 h-10 rounded-full cursor-pointer'
+                            src={`/${setProfileImage()}`}
+                            alt="profileImg"
+                        />
                     </Link>
                 </div>
             </div>
